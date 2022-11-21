4b5d2a624322ae237c8776b8fc7d6c3c04b56d85
Oleh Rizun <vinyloleg228@gmail.com>
2022-11-21 11:35:48 +0200

    Hillel

--- src/HillelRizunDz5/ArmyPower.java
+++ src/HillelRizunDz5/ArmyPower.java
@@ -1,10 +1,27 @@
 package HillelRizunDz5;
 
 public class ArmyPower {
-
-fjsodfnskdf;
-
     public static void main(String[] args) {
+        int warrior = 13;
+        int archers = 24;
+        int riders = 46;
 
+        int LiArmy = ((warrior + archers + riders)*860);
+        System.out.println(LiArmy);
+
+
+        int warrior1 = 9;
+        int archers1 = 35;
+        int riders1 = 12;
+        int amountOfArmy = 860;
+        double training = (860*1.5);
+//        System.out.println(training);
+
+        double MinArmy = ((warrior1 + archers1 + riders1)*training);
+        System.out.println(MinArmy);
     }
 }
+
+
+
+
