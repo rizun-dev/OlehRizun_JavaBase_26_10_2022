ac73b709b49ba54a988bb2c8f1faea0c764fefee
Oleh <vinyloleg228@gmail.com>
2022-12-12 16:40:14 +0200

    Hillel

--- src/HillelRizunDz9/regbi.java
+++ src/HillelRizunDz9/regbi.java
@@ -1,6 +1,7 @@
 package HillelRizunDz9;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class regbi {
 
@@ -12,24 +13,34 @@ public class regbi {
 //        Порахуйте середній вік гравців кожної команди та виведіть на екран.
 
 
-        int[] team1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+        int[] team1 = new int [25];
         int[] team2 = new int[team1.length];
+        int sum = 0;
+        int sum1 = 0;
+        double total = 0;
+        double total1 = 0;
+        double total2 = 0;
+        double total3 = 0;
 
         for (int i = 0; i < team1.length; i++) {
             team1[i] = (int) (Math.random() * 22 + 18);
-            for (int j = 0; j < team2.length; j++) {
-                team2[j] = (int) (Math.random() * 22 + 18);
-                team1[i] = team2 [j];
-            }
+            total = sum += team1[i];
+            total1 = total / team1.length;
         }
-        System.out.println("Team1");
+        System.out.println("\n" + "Team1");
         System.out.print(Arrays.toString(team1));
+        System.out.println("\n" + total);
+        System.out.println(total1);
 
-
+        for (int j = 0; j < team2.length; j++) {
+            team2[j] = (int) (Math.random() * 22 + 18);
+        total2 = sum1 += team1[j];
+        total3 = total2 / team1.length;
+        }
         System.out.println("\n" + "Team2");
         System.out.print(Arrays.toString(team2));
-
-
+        System.out.println("\n" + total2);
+        System.out.println(total3);
     }
 }
 
