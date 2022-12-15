3ecc30d76c658d27c7e40e7a72dea789b284ebd5
Oleh <vinyloleg228@gmail.com>
2022-12-15 22:03:43 +0200

    Hillel

--- src/HillelRizunDz11/Matrix.java
+++ src/HillelRizunDz11/Matrix.java
@@ -3,70 +3,81 @@ package HillelRizunDz11;
 import java.util.Scanner;
 
 
-    public class Matrix {
-        public static void main(String[] args) {
+public class Matrix {
+    public static void main(String[] args) {
 
 //            Транспонуйте матрицю M x N. Розміри M та N задаються з консолі.
 //            Після введення повинні автоматично створюватися два масиви розміром M x N перший та N x M другий.
 
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
-            int[][] sourceMatrix = new int[x][y];
-            int[][] transposedMatrix = new int[y][x];
-            int numberFiller = 1;
+        int x;
+        int y;
 
-            for (int i = 0; i < sourceMatrix.length; i++) {
-                for (int j = 0; j < sourceMatrix[i].length; j++) {
-                    sourceMatrix[i][j] = numberFiller++;
-                    System.out.print(sourceMatrix[i][j] + " ");
-                    while (true) {
-                        if (scanner.hasNextInt()) {
-                            sourceMatrix[i][j] = scanner.nextInt();
-                            System.out.println(sourceMatrix[i][j]);
-                            break;
-                        } else {
-                            System.out.println("Wrong date");
-                            scanner.nextLine();
-                        }
+        System.out.println("Entered matrix size");
+
+        while (true) {
+            System.out.print("x = ");
+            if (scanner.hasNextInt()) {
+                x = scanner.nextInt();
+                break;
+            } else {
+                System.out.println("Wrong date");
+                scanner.next();
+            }
+        }
+
+        while (true) {
+            System.out.print("y = ");
+            if (scanner.hasNextInt()) {
+                y = scanner.nextInt();
+                break;
+            } else {
+                System.out.println("Wrong date");
+                scanner.next();
+            }
+        }
+
+        int[][] mainMatrix = new int[x][y];
+        int[][] transposedMatrix = new int[y][x];
+        int numberFiller = 1;
+
+        for (int i = 0; i < mainMatrix.length; i++) {
+            for (int j = 0; j < mainMatrix[i].length; j++) {
+                mainMatrix[i][j] = numberFiller++;
+                while (true) {
+                    System.out.print("Input number : ");
+                    if (scanner.hasNextInt()) {
+                        mainMatrix[i][j] = scanner.nextInt();
+                        break;
+                    } else {
+                        System.out.println("Wrong date");
+                        scanner.next();
                     }
                 }
-                System.out.println();
             }
+            System.out.println();
+        }
 
-            for (int[] ints : sourceMatrix) {
-                for (int anInt : ints) {
-                    System.out.print(anInt + "\t");
-                }
-                System.out.println();
+        for (int[] ints : mainMatrix) {
+            for (int i : ints) {
+                System.out.print(i + "\t");
             }
+            System.out.println();
+        }
 
-            for (int i = 0; i < sourceMatrix.length; i++) {
-                for (int j = 0; j < sourceMatrix[i].length; j++) {
-                    transposedMatrix[j][i] = sourceMatrix[i][j];
-                }
-                System.out.println();
+        for (int i = 0; i < mainMatrix.length; i++) {
+            for (int j = 0; j < mainMatrix[i].length; j++) {
+                transposedMatrix[j][i] = mainMatrix[i][j];
             }
+            System.out.println();
+        }
 
-            for (int[] matrix : transposedMatrix) {
-                for (int i : matrix) {
-                    System.out.print(i + " ");
-                }
-                System.out.println();
+        for (int[] matrix : transposedMatrix) {
+            for (int i : matrix) {
+                System.out.print(i + " ");
             }
-
-
-
-
-
+            System.out.println();
         }
     }
-
-
-
-
-
-
-
-
+}
