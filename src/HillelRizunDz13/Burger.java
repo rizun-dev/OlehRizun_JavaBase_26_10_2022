03e1fc1ac3506aa73b05c99facc04e7f6f475bc3
Oleh <vinyloleg228@gmail.com>
2022-12-26 15:00:19 +0200

    Hillel

---
+++ src/HillelRizunDz13/Burger.java
@@ -0,0 +1,45 @@
+package HillelRizunDz13;
+
+public class Burger {
+
+//    Main - Зробити клас Burger, який матиме поля:
+//    булочка, м'ясо, сир, зелень, майонез
+
+//        1. Зробити три конструктори, які дозволятимуть робити бургери трьох видів:
+//          а)звичайний бургер (усі компоненти)
+//          б)дієтичний бургер (без майонезу)
+//          в)з подвійним м'ясом
+//        2. У кожен конструктор додати виведення в консоль складу даного варіанту бургера (при створенні екземпляра бургера,
+//        який використовує даний конструктор - буде виводитися в консоль його склад).
+//        3. Зробити клас BurgerMain, в якому створити три різні бургери. Їхній склад буде автоматично виводитися на екран під час запуску програми.
+
+    static String bun = "bun";
+    static String meat = "meat";
+    static String cheese = "cheese";
+    static String salad = "salad";
+    static String mayonnaise = "mayonnaise";
+    static String secondMeat = "second meat";
+
+    public static void main(String[] args) {
+        BurgerClass classicMac = new BurgerClass(bun, meat, cheese, salad, mayonnaise);
+        BurgerClass noMayonnaiseMac = new BurgerClass(bun, meat, cheese, salad);
+        BurgerClass doubleMac = new BurgerClass(bun, meat, cheese, salad, mayonnaise, secondMeat);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
